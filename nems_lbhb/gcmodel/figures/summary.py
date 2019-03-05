fixed bug related to dynamic_sigmoid normalization

nems_lbhb/gcmodel/figures/summary.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy.stats as st
 
 import nems.db as nd
-from .utils import get_valid_improvements
+from nems_lbhb.gcmodel.figures.utils import get_valid_improvements
 
 
 gc_color = '#69657C'

nems_lbhb/gcmodel/modules.py
@@ -35,8 +35,9 @@ def dynamic_sigmoid(rec, i, o, c, base, amplitude, shift, kappa,
 
     if not static and rec[c]:
         contrast = rec[c].as_continuous()
+
         if norm:
-            contrast = contrast/contrast.max()
+            contrast = contrast/np.nanmax(contrast)
 
         if np.isnan(base_mod):
             base_mod = base