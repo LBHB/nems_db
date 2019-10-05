a2535d2c0fafad043fd8d7e49cc07790a1a44d57
crheller <charlieheller95@gmail.com>
2019-10-05 15:14:38 -0700

    tweak to behavior demo

--- nems_lbhb/scripts/behavior_analysis_demo.py
+++ nems_lbhb/scripts/behavior_analysis_demo.py
@@ -2,17 +2,16 @@ import nems_lbhb.behavior as beh
 import matplotlib.pyplot as plt
 import numpy as np
 parmfile = '/auto/data/daq/Drechsler/training2019/Drechsler_2019_10_03_BVT_4.m'  
-options = {}
+options = {'valid_trials': True}
 # plot an example RT histogram
 RT = beh.get_RT(parmfile, **options)
 HR = beh.get_HR_FAR(parmfile, **options)
 
 bins = np.arange(0, 1, 0.05)
 f, ax = plt.subplots(1, 1)
-for k in RT.keys(): 
+for k, hr_key in zip(RT.keys(), HR.keys()): 
     counts, xvals = np.histogram(RT[k], bins=bins)
-
-    ax.step(xvals[:-1], np.cumsum(counts) / len(RT[k]) * HR[k], label=k)
+    ax.step(xvals[:-1], np.cumsum(counts) / len(RT[k]) * HR[hr_key], label=k)
 
 ax.legend()
 ax.set_xlabel('Reaction time')
