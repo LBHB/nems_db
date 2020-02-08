d871a0194b97c0791d2ead4460b139bb9bdbe5a8
Stephen David <stephen.v.david@gmail.com>
2020-02-08 09:27:19 -0800

    tweaked evs keyword to work with cc data

--- nems_lbhb/plugins/lbhb_preprocessors.py
+++ nems_lbhb/plugins/lbhb_preprocessors.py
@@ -76,16 +76,24 @@ def evs(loadkey):
     # TODO: implement better parser for more flexibility
     loadset = loader.split(".")
 
-    if loader == ("tar.lic"):
+    if loadset[0]=='tar':
+        epoch_regex='^TAR_'
+    elif loadset[0]=='cct':
+        epoch_regex = '^[A-Za-z]+_[0-9]+$'
+    else:
+        raise ValueError('unknown stim spec')
+
+
+    if loadset[1] == "lic":
         epoch2_shuffle = False
-    elif loader == ("tar.lic0"):
+    elif loadset[1] == "lic0":
         epoch2_shuffle = True
     else:
-        raise ValueError("unknown signals for alt-stimulus initializer")
+        raise ValueError("unknown lic spec")
 
     xfspec = [['nems.preprocessing.generate_stim_from_epochs',
                {'new_signal_name': 'stim',
-                'epoch_regex': '^TAR_', 'epoch_shift': 5,
+                'epoch_regex': epoch_regex, 'epoch_shift': 5,
                 'epoch2_regex': 'LICK', 'epoch2_shift': -5,
                 'epoch2_shuffle': epoch2_shuffle, 'onsets_only': True},
                ['rec'], ['rec']],
