297acd9f540b058ad729d9051c235b8fda1b65c1
Stephen David <stephen.v.david@gmail.com>
2020-02-12 10:39:38 -0800

    tidying example script

--- nems_lbhb/scripts/cc_learning/load_model_fits.py
+++ nems_lbhb/scripts/cc_learning/load_model_fits.py
@@ -1,5 +1,8 @@
 
 import os
+import glob
+import numpy as np
+
 from nems.xforms import load_analysis
 
 """
@@ -20,19 +23,24 @@ export_fits(315,
 # datapath should indicate where the NEMS models have been saved
 datapath = '/auto/users/svd/projects/reward_training/nems_export/torcs/'
 
-d = os.listdir(datapath)
+# find all models, knowing that the folder names should contain "2020"
+d = glob.glob(datapath + '*2020*')
 
 # get unique list of cells and modelnames
 cellids = list(set([f.split("_")[0] for f in d]))
 modelnames = list(set([f.split("_")[1].split('.2020')[0] for f in d]))
 
-
 # load an example model
-i=163
-xf, ctx = load_analysis(os.path.join(datapath,d[i]), eval_model=False)
+cellid = 'NMK020c-29-1'
+modelinfo = 'st.pup.fil-'
+
+i, = np.where([(cellid in f) and (modelinfo in f) for f in d])
+i = i[0]   # is an array, just take first entry (should just be 1)
+
+xf, ctx = load_analysis(d[i], eval_model=False)
 
 modelspec=ctx['modelspec']
-print('Loaded model {}'.format(d[i]))
+print('Loaded model {}'.format(os.path.basename(d[i])))
 print('Cellid: {}\nModel name: {}'.format(modelspec.meta['cellid'],
                                           modelspec.meta['modelname']))
 print('r_test: {:.3f}'.format(modelspec.meta['r_test'][0][0]))
