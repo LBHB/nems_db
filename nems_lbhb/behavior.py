fa6831258115a68facd0f9d04a8b4c8a115950cd
crheller <charlieheller95@gmail.com>
2020-02-13 17:11:06 -0800

    fix some behavior bugs and add plotting functions

--- nems_lbhb/behavior.py
+++ nems_lbhb/behavior.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 import pandas as pd
 import nems_lbhb.io as io
 from itertools import combinations
+import matplotlib.pyplot as plt
 import logging
 
 log = logging.getLogger(__name__)
@@ -34,7 +35,7 @@ def create_trial_labels(exptparams, exptevents):
     Also, add two columns to number and classify each individual sound as its own trial
         - For example, each REF can be CORRECT_REJECT or FALSE_ALARM etc.
         - For sounds that weren't played (for ex because of lick early on causing a FA),
-            label them NULL and give them trial number 0.
+            label them NULL and dont give them a trial number.
     """
 
     all_trials = np.unique(exptevents['Trial'])
@@ -145,7 +146,7 @@ def create_trial_labels(exptparams, exptevents):
                     ev.loc[ev.name==baphy_outcome, 'name'] = 'FALSE_ALARM_TRIAL'
                     # if this is the case, also update the last soundTrial
                     soundIDX = ev[ev.soundTrial != 'NULL'].index[-1]
-                    ev.set_value(soundIDX, 'soundTrial', 'EARLY_TRIAL')
+                    ev.at[soundIDX, 'soundTrial'] = 'EARLY_TRIAL'
                 else:
                     ev.loc[ev.name==baphy_outcome, 'name'] = trial_outcome
             trial_dfs.append(ev)
@@ -177,7 +178,13 @@ def create_trial_labels(exptparams, exptevents):
             trial_dfs.append(ev)
 
     new_events = pd.concat(trial_dfs)
-        
+
+    # finally, number the (non NULL) sound trials
+    new_events['soundTrialidx'] = 0
+    trialidx = np.arange(1, (new_events.soundTrial!='NULL').sum()+1)
+    #new_events['soundTrialidx'][new_events.soundTrial!='NULL'] = trialidx
+    new_events.at[new_events.soundTrial!='NULL', 'soundTrialidx'] = trialidx
+
     return new_events
 
 
@@ -194,6 +201,12 @@ def mark_invalid_trials(exptparams, exptevents, **options):
         - keep_follwing_incorrect_trial: default = False
                 default, exclude trials that follow incorrect trials e.g. miss of rewarded target, false alarm, hit of unrewarded target.
                 If True, treat each trial as normal.
+        - trial_numbers: default = None
+                if not None, keep only trial numbers contained in trial_numbers
+                can be list, np.array, tuple...
+        - sound_trial_numbers: default = None
+                if not None, keep only sound trial numbers contained in trial_numbers
+                can be list, np.array, tuple...
     """
     
     events = exptevents.copy()
@@ -207,6 +220,8 @@ def mark_invalid_trials(exptparams, exptevents, **options):
     keep_early_trials = options.get('keep_early_trials', False)
     keep_cue_trials = options.get('keep_cue_trials', False)
     keep_following_incorrect_trial = options.get('keep_following_incorrect_trial', False)
+    trial_numbers = options.get('trial_numbers', None)
+    sound_trial_numbers = options.get('sound_trial_numbers', None)
 
     iv_trials = False * np.ones(events.shape[0]).astype(np.bool)
     iv_sound_trials = False * np.ones(events.shape[0]).astype(np.bool)
@@ -223,6 +238,8 @@ def mark_invalid_trials(exptparams, exptevents, **options):
     if keep_cue_trials == False:
         # mark cue trials as invalid
         # same for single sounds and overall trials
+        # TO-DO add logic to use new structure in exptparams['TrialParams'][1]['CueSeg']
+        # to determine cue trials for newer baphy files (after Luke's modifications)
         nCueTrials = exptparams['TrialObject'][1]['CueTrialCount']
         if nCueTrials > 0:
             cue_hit_trials = events[events.name.str.contains('HIT_TRIAL')]['Trial'][:(nCueTrials+1)].values
@@ -238,6 +255,17 @@ def mark_invalid_trials(exptparams, exptevents, **options):
         following_trials = incorrect_trials + 1
         iv = events.Trial.isin(following_trials)
         iv_trials = iv | iv_trials
+    
+    if trial_numbers is not None:
+        # mark all trial not in trial_numbers as invalid
+        iv = (events.Trial.isin(trial_numbers) == False)
+        iv_trials = iv | iv_trials
+        iv_sound_trials = iv | iv_sound_trials
+    
+    if sound_trial_numbers is not None:
+        # mark all trial not in trial_numbers as invalid
+        iv = (events.soundTrialidx.isin(sound_trial_numbers) == False)
+        iv_sound_trials = iv | iv_sound_trials
 
     # Mark any REFs that occur in an invalid target slot as invalid sounds
     # so that they won't be used for DI / RT calculations
@@ -252,6 +280,17 @@ def mark_invalid_trials(exptparams, exptevents, **options):
 
     events.insert(events.shape[-1], 'invalidSoundTrial', iv_sound_trials)
     events.insert(events.shape[-1], 'invalidTrial', iv_trials)    
+
+    # double check that any trials where all the sounds were invalid are
+    # also marked as invalid
+    iv_trials_based_on_sound = []
+    for i in events.Trial.unique():
+        evt = events[events.Trial==i]
+        if evt['invalidSoundTrial'].sum() == evt.shape[0]:
+            iv_trials_based_on_sound.append(i)
+
+    iv = events.Trial.isin(iv_trials_based_on_sound)
+    events['invalidTrial'] =  iv | events['invalidTrial']
 
     return events
 
@@ -314,23 +353,37 @@ def _compute_metrics(exptparams, exptevents):
         if rewarded:
             # looking for "HIT_TRIALS" and "MISS_TRIALS"
             allTarTrials = exptevents[(exptevents.name==tar)]['Trial']
+            #validTrialList = exptevents[exptevents.Trial.isin(allTarTrials) & \
+            #                            (exptevents.invalidTrial==False) & \
+            #                            (exptevents.name.isin(['HIT_TRIAL', 'MISS_TRIAL', 'CUE_TRIAL']))]['Trial']
             validTrialList = exptevents[exptevents.Trial.isin(allTarTrials) & \
+                                        (exptevents.invalidSoundTrial==False) & \
                                         (exptevents.invalidTrial==False) & \
-                                        (exptevents.name.isin(['HIT_TRIAL', 'MISS_TRIAL', 'CUE_TRIAL']))]['Trial']
+                                        (exptevents.soundTrial.isin(['HIT_TRIAL', 'MISS_TRIAL', 'CUE_TRIAL']))]['Trial']
             nTrials = len(np.unique(validTrialList))
             validTrialdf = exptevents[exptevents.Trial.isin(validTrialList)]
             nHits = (validTrialdf.name=='HIT_TRIAL').sum()
-            R['RR'][tar_key] = nHits / nTrials
+            if nTrials == 0:
+                R['RR'][tar_key] = np.nan
+            else:
+                R['RR'][tar_key] = nHits / nTrials
         else:
             # looking for "INCORRECT_HIT_TRIALS" and "CORRECT_REJECT_TRIALS"
             allTarTrials = exptevents[(exptevents.name==tar)]['Trial']
+            #validTrialList = exptevents[exptevents.Trial.isin(allTarTrials) & \
+            #                            (exptevents.invalidTrial==False) & \
+            #                            (exptevents.name.isin(['CORRECT_REJECT_TRIAL', 'INCORRECT_HIT_TRIAL']))]['Trial']
             validTrialList = exptevents[exptevents.Trial.isin(allTarTrials) & \
+                                        (exptevents.invalidSoundTrial==False) & \
                                         (exptevents.invalidTrial==False) & \
-                                        (exptevents.name.isin(['CORRECT_REJECT_TRIAL', 'INCORRECT_HIT_TRIAL']))]['Trial']
+                                        (exptevents.soundTrial.isin(['CORRECT_REJECT_TRIAL', 'INCORRECT_HIT_TRIAL']))]['Trial']
             nTrials = len(np.unique(validTrialList))
             validTrialdf = exptevents[exptevents.Trial.isin(validTrialList)]
             nHits = (validTrialdf.name=='INCORRECT_HIT_TRIAL').sum()
-            R['RR'][tar_key] = nHits / nTrials
+            if nTrials == 0:
+                R['RR'][tar_key] = np.nan
+            else:
+                R['RR'][tar_key] = nHits / nTrials
 
         R['nTrials'][tar_key] = nTrials
 
@@ -344,7 +397,11 @@ def _compute_metrics(exptparams, exptevents):
         validTrialdf = exptevents[exptevents.Trial.isin(validTrialList)]
         nTrials = sum((validTrialdf.invalidSoundTrial==False) & (validTrialdf.soundTrial.isin(['FALSE_ALARM_TRIAL', 'EARLY_TRIAL', 'CORRECT_REJECT_TRIAL'])))
         nFA = ((validTrialdf.invalidSoundTrial==False) & ((validTrialdf.soundTrial=='FALSE_ALARM_TRIAL') | (validTrialdf.soundTrial=='EARLY_TRIAL'))).sum()
-        R['RR']['Reference'] = nFA / nTrials
+        if nTrials == 0:
+            R['RR']['Reference'] = np.nan
+        else:
+            R['RR']['Reference'] = nFA / nTrials
+
         R['nTrials']['Reference'] = nTrials
 
         # Use the HRs above to compute d' values
@@ -383,8 +440,15 @@ def _compute_DI(tar_RTs, tarHR, ref_RTs, FAR, resp_window, dx=0.1):
     # compute response probability in each bin
     tar_counts, _ = np.histogram(tar_RTs, bins=bins)
     ref_counts, _ = np.histogram(ref_RTs, bins=bins)
-    tar_prob = (np.cumsum(tar_counts) / sum(tar_counts)) * tarHR
-    ref_prob = (np.cumsum(ref_counts) / sum(ref_counts)) * FAR    
+    if sum(tar_counts) > 0:
+        tar_prob = (np.cumsum(tar_counts) / sum(tar_counts)) * tarHR
+    else:
+        tar_prob = np.nan
+    
+    if sum(ref_counts) > 0:
+        ref_prob = (np.cumsum(ref_counts) / sum(ref_counts)) * FAR    
+    else:
+        ref_prob = np.nan
 
     # force the area bounded by the ROC curve to end at (1, 1)
     tar_RT_prob = np.append(tar_prob, 1)
@@ -531,4 +595,189 @@ def _get_reference_RTs(exptparams, exptevents):
 
 
 
+# =================================== Plotting functions ==========================================
+
+def plot_RT_hist(exptparams, exptevents, ax=None, bins=None, **trial_options):
+
+    if ax is None:
+        _, ax = plt.subplots(1, 1)
     
+    # get behavior metrics (for the response rate)
+    metrics = compute_metrics(exptparams, exptevents, **trial_options)
+
+    # get reaction times
+    RTs = compute_RTs(exptparams, exptevents, **trial_options)
+
+    # set bins for histogram
+    if bins is None:
+        bins = np.arange(0, 1, 0.1)
+
+    skeys = np.sort(list(RTs.keys()))
+
+    # Now, for each soundID (reference, target1, target2 etc.),
+    # create histogram
+    for k in skeys:
+        counts, xvals = np.histogram(RTs[k], bins=bins)
+        try:
+            DI = round(metrics['DI'][k], 2)
+        except:
+            DI = 'N/A'
+        HR = round(metrics['RR'][k], 2)
+        n = metrics['nTrials'][k]
+        ax.step(xvals[:-1], 
+                    np.cumsum(counts) / len(RTs[k]) * metrics['RR'][k], 
+                    label='{0}, DI: {1}, HR: {2}, n: {3}'.format(k, DI, HR, n), lw=2)
+    
+    ax.legend(fontsize=6, frameon=False)
+    ax.set_xlabel('Reaction time', fontsize=8)
+    ax.set_ylabel('HR', fontsize=8)
+    ax.set_title("RT histogram", fontsize=8)
+
+    return ax
+
+
+def plot_cummulative_HR(exptparams, exptevents, ax=None, annotate=False, **trial_options):
+    '''
+    Loop through events and plot the HR for each sound. Take steps of one (sound) trial. 
+    A point gets added to each curve every time a new sound of that type occurs and
+    the HR is based on what happened during all previous SOUND trials (not baphy trials)
+    
+    if annotate==True
+        run _get_marker to find appropriate marker based on trial type
+
+    will plot everything (all trials), but only update the HR value by considering VALID sounds
+    '''
+
+    # get range of sound trials
+    soundTrials = np.sort(exptevents.soundTrialidx.unique())[1:]
+
+    # this should always be done on a "per sound trial basis", 
+    # so we will force trial_numbers (BAPHY trials) to None (meaning 
+    # that the only trial data mask will be based on sound trials)
+    trial_options.update({'trial_numbers': None})
+
+    # compute metrics at first sound trial
+    trial_options.update({'sound_trial_numbers': np.arange(1, 2)})
+    metrics = compute_metrics(exptparams, exptevents, **trial_options)
+
+    skeys = np.sort(list(metrics['RR'].keys()))
+
+    HR = {k: [] for k in skeys}
+    markers = {k: [] for k in skeys} 
+    facecolor = {k: [] for k in skeys} 
+    edgecolor = {k: [] for k in skeys}
+    sound_trial_n = {k: [] for k in skeys}
+    mpl_defaults = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    sound = exptevents[exptevents.soundTrialidx==1]['name'].values[0]
+    trial_type = exptevents[exptevents.soundTrialidx==1]['soundTrial'].values[0]
+
+    log.info('Calculating HRs for trial 1 / {0}'.format(len(soundTrials)))
+    for i, k in enumerate(HR.keys()):
+        # check if key was present on this trial. If not, don't append.
+        if (k in sound) & (np.isnan(metrics['RR'][k])==False):
+            HR[k].append(metrics['RR'][k])
+            m = _get_marker(trial_type)
+            c = mpl_defaults[i]
+            if (trial_type == 'MISS_TRIAL') | (trial_type == 'CORRECT_REJECT_TRIAL'):
+                # empty marker for miss trials
+                fc = 'none'
+            else:
+                fc = c
+            markers[k].append(m)
+            facecolor[k].append(fc)
+            edgecolor[k].append(c)
+            sound_trial_n[k].append(1)
+
+    # Now that HR dict is initialized, loop over remaining sound trials 
+    # and update
+    for t in soundTrials[2:]:
+        if t % 50 == 0:
+            log.info('Calculating HRs for trial {0} / {1}'.format(t, len(soundTrials)))
+        sound = exptevents[exptevents.soundTrialidx==t]['name'].values[0]
+        trial_type = exptevents[exptevents.soundTrialidx==t]['soundTrial'].values[0]
+        
+        # compute cummulative metrics
+        trial_options.update({'sound_trial_numbers': np.arange(1, t)})
+        metrics = compute_metrics(exptparams, exptevents, **trial_options)
+
+        # loop over keys to see which to update
+        for i, k in enumerate(HR.keys()):
+            # check if key was present on this trial and if there's valid data
+            # If not, don't append.
+            if (k in sound) & (np.isnan(metrics['RR'][k])==False):
+                HR[k].append(metrics['RR'][k])
+                m = _get_marker(trial_type)
+                c = mpl_defaults[i]
+                if (trial_type == 'MISS_TRIAL') | (trial_type == 'CORRECT_REJECT_TRIAL'):
+                    # empty marker for miss trials
+                    fc = 'none'
+                else:
+                    fc = c
+                markers[k].append(m)
+                facecolor[k].append(fc)
+                edgecolor[k].append(c)
+                sound_trial_n[k].append(t-1)
+    
+    # generate figure based on HR, markers, facecolor, and edgecolor dictionaries
+    if ax is None:
+        _, ax = plt.subplots(1, 1)
+    
+    for k in HR.keys():        
+        if annotate:
+            ax.plot(sound_trial_n[k], HR[k], alpha=0.5, label=k)
+            mscatter(sound_trial_n[k], HR[k], m=markers[k], color=facecolor[k], edgecolor=edgecolor[k], s=25, ax=ax)
+        else:
+            ax.plot(sound_trial_n[k], HR[k], '.-', alpha=0.5, label=k)
+    
+    ax.set_xlabel('Trial', fontsize=8)
+    ax.set_ylabel('Hit Rate', fontsize=8)
+    ax.legend(frameon=False, fontsize=8)
+
+    return ax
+
+
+def _get_marker(soundTrial):
+    '''
+        open circle = miss
+        closed circle = hit
+        + = early response 
+        * = False alarm
+        open square = correct reject
+        closed square = incorrect hit
+    '''
+
+    if soundTrial=='MISS_TRIAL':
+        marker = 'o'
+    elif soundTrial=='HIT_TRIAL':
+        marker = 'o'
+    elif soundTrial=='EARLY_TRIAL':
+        marker = '+'
+    elif soundTrial=='CORRECT_REJECT_TRIAL':
+        marker = 's'
+    elif soundTrial=='INCORRECT_HIT_TRIAL':
+        marker = 's'
+    elif soundTrial=='FALSE_ALARM_TRIAL':
+        marker = '*'
+    else:
+        marker = 'x'
+
+    return marker
+
+
+
+def mscatter(x, y, ax=None, m=None, **kw):
+    import matplotlib.markers as mmarkers
+    if not ax: ax=plt.gca()
+    sc = ax.scatter(x,y,**kw)
+    if (m is not None) and (len(m)==len(x)):
+        paths = []
+        for marker in m:
+            if isinstance(marker, mmarkers.MarkerStyle):
+                marker_obj = marker
+            else:
+                marker_obj = mmarkers.MarkerStyle(marker)
+            path = marker_obj.get_path().transformed(
+                        marker_obj.get_transform())
+            paths.append(path)
+        sc.set_paths(paths)
+    return sc
