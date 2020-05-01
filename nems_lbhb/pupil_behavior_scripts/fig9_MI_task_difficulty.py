7696a1a75e9daac68206c0e473a96c6f2c1973d2
crheller <charlieheller95@gmail.com>
2020-04-30 17:43:06 -0700

    breaking out difficulty into three categories

--- nems_lbhb/pupil_behavior_scripts/fig9_MI_task_difficulty.py
+++ nems_lbhb/pupil_behavior_scripts/fig9_MI_task_difficulty.py
@@ -19,7 +19,8 @@ r0_threshold = 0.5
 octave_cutoff = 0.5
 yaxis = 'MI_task_unique'  # MI_task_unique, MI_task (task only)
 sig_col = 'sig_utask'     # sig_utask (sig unique task effect), sig_task (sig task only), sig_state (sig state effect)
-easy = [0,1]             # pure-tone = 0, low SNR = 1, high SNR = 3
+easy = [0]             # pure-tone = 0, low SNR = 1, high SNR = 3
+medium = [1]
 hard = [3]
 
 AFL = True
@@ -59,8 +60,8 @@ IC = helper.preprocess_sdexp_dump(dump_results,
 IC = IC[IC.sig_psth]
 
 # convert difficulty to True (for hard) and False (for easy)
-A1['difficulty'] = [True if x in hard else False for x in A1.difficulty]
-IC['difficulty'] = [True if x in hard else False for x in IC.difficulty]
+#A1['difficulty'] = [True if x in hard else False for x in A1.difficulty]
+#IC['difficulty'] = [True if x in hard else False for x in IC.difficulty]
 
 # stripplot of MI split by difficulty and task significance
 f, ax = plt.subplots(1, 2, figsize=(5, 3), sharey='row')
@@ -69,32 +70,46 @@ sns.stripplot(x=sig_col, y=yaxis, data=A1, hue='difficulty', dodge=True, edgecol
                         marker='o', size=5, ax=ax[0])
 ax[0].axhline(0, linestyle='--', lw=2, color='grey')
 
-pval = round(ss.ranksums(A1[A1.difficulty & A1[sig_col]][yaxis], A1[~A1.difficulty & A1[sig_col]][yaxis]).pvalue, 3)
-off_med = round(A1[~A1.difficulty & A1[sig_col]][yaxis].median(), 3)
-on_med = round(A1[A1.difficulty & A1[sig_col]][yaxis].median(), 3)
+# medians
+easy_med = round(A1[A1.difficulty.isin(easy) & A1[sig_col]][yaxis].median(), 3)
+medium_med = round(A1[A1.difficulty.isin(medium) & A1[sig_col]][yaxis].median(), 3)
+hard_med = round(A1[A1.difficulty.isin(hard) & A1[sig_col]][yaxis].median(), 3)
 
-pval_ns = round(ss.ranksums(A1[A1.difficulty & ~A1[sig_col]][yaxis], A1[~A1.difficulty & ~A1[sig_col]][yaxis]).pvalue, 3)
-off_med_ns = round(A1[~A1.difficulty & ~A1[sig_col]][yaxis].median(), 3)
-on_med_ns = round(A1[A1.difficulty & ~A1[sig_col]][yaxis].median(), 3)
+# pvals 
+easy_v_medium = round(ss.ranksums(A1[A1.difficulty.isin(easy) & A1[sig_col]][yaxis], A1[A1.difficulty.isin(medium) & A1[sig_col]][yaxis]).pvalue, 3)
+medium_v_hard = round(ss.ranksums(A1[A1.difficulty.isin(medium) & A1[sig_col]][yaxis], A1[A1.difficulty.isin(hard) & A1[sig_col]][yaxis]).pvalue, 3)
+easy_v_hard = round(ss.ranksums(A1[A1.difficulty.isin(easy) & A1[sig_col]][yaxis], A1[A1.difficulty.isin(hard) & A1[sig_col]][yaxis]).pvalue, 3)
 
-ax[0].set_title('A1 \nsig_cells: HARD: {0}, EASY: {1}, p: {2}\n'
-                'ns cells: HARD: {3}, EASY: {4}, p: {5}'.format(on_med, off_med, pval, on_med_ns, off_med_ns, pval_ns))
+ax[0].set_title('A1 \n HARD: {0}, MEDIUM: {1}, EASY: {2}, \n'
+                'p_em = {3}, p_mh = {4}, p_eh = {5}'.format(easy_med,
+                                                            medium_med,
+                                                            hard_med,
+                                                            easy_v_medium,
+                                                            medium_v_hard,
+                                                            easy_v_hard))
 nplt.ax_remove_box(ax[0])
 
 sns.stripplot(x=sig_col, y=yaxis, data=IC, hue='difficulty', dodge=True, edgecolor='white', linewidth=0.5,
                         marker='o', size=5, ax=ax[1])
 ax[1].axhline(0, linestyle='--', lw=2, color='grey')
 
-pval = round(ss.ranksums(IC[IC.difficulty & IC[sig_col]][yaxis], IC[~IC.difficulty & IC[sig_col]][yaxis]).pvalue, 3)
-off_med = round(IC[~IC.difficulty & IC[sig_col]][yaxis].median(), 3)
-on_med = round(IC[IC.difficulty & IC[sig_col]][yaxis].median(), 3)
+# medians
+easy_med = round(IC[IC.difficulty.isin(easy) & IC[sig_col]][yaxis].median(), 3)
+medium_med = round(IC[IC.difficulty.isin(medium) & IC[sig_col]][yaxis].median(), 3)
+hard_med = round(IC[IC.difficulty.isin(hard) & IC[sig_col]][yaxis].median(), 3)
 
-pval_ns = round(ss.ranksums(IC[IC.difficulty & ~IC[sig_col]][yaxis], IC[~IC.difficulty & ~IC[sig_col]][yaxis]).pvalue, 3)
-off_med_ns = round(IC[~IC.difficulty & ~IC[sig_col]][yaxis].median(), 3)
-on_med_ns = round(IC[IC.difficulty & ~IC[sig_col]][yaxis].median(), 3)
+# pvals 
+easy_v_medium = round(ss.ranksums(IC[IC.difficulty.isin(easy) & IC[sig_col]][yaxis], IC[IC.difficulty.isin(medium) & IC[sig_col]][yaxis]).pvalue, 3)
+medium_v_hard = round(ss.ranksums(IC[IC.difficulty.isin(medium) & IC[sig_col]][yaxis], IC[IC.difficulty.isin(hard) & IC[sig_col]][yaxis]).pvalue, 3)
+easy_v_hard = round(ss.ranksums(IC[IC.difficulty.isin(easy) & IC[sig_col]][yaxis], IC[IC.difficulty.isin(hard) & IC[sig_col]][yaxis]).pvalue, 3)
 
-ax[1].set_title('IC \nsig_cells: HARD: {0}, EASY: {1}, p: {2}\n'
-                'ns cells: HARD: {3}, EASY: {4}, p: {5}'.format(on_med, off_med, pval, on_med_ns, off_med_ns, pval_ns))
+ax[1].set_title('IC \n HARD: {0}, MEDIUM: {1}, EASY: {2}, \n'
+                'p_em = {3}, p_mh = {4}, p_eh = {5}'.format(easy_med,
+                                                            medium_med,
+                                                            hard_med,
+                                                            easy_v_medium,
+                                                            medium_v_hard,
+                                                            easy_v_hard))
 nplt.ax_remove_box(ax[1])
 f.tight_layout()
 
@@ -102,7 +117,7 @@ f.tight_layout()
 if save_fig:
     f.savefig(os.path.join(save_path, 'fig9_difficulty_percell.pdf'))
 
-# only look at cells that were recorded in both conditions
+# only look at cells that were recorded in all conditions
 f, ax = plt.subplots(2, 2, figsize=(6, 8))
 
 # significant cells only
@@ -111,20 +126,20 @@ mi = A1g[[yaxis]]
 cells = []
 for c in mi.index.get_level_values('cellid').unique():
     y = mi.loc[pd.IndexSlice[c, :], :].values
-    if len(y) > 1:
-        ax[0, 0].plot([0, 1], y, 'k', alpha=0.3)
+    if len(y) == 3:
+        ax[0, 0].plot([0, 1, 2], y, 'k', alpha=0.3)
         cells.append(c)
-ax[0, 0].plot([0, 1], mi.loc[cells].groupby(by='difficulty').mean(), 'o-', lw=2, color='k')
-ax[0, 0].set_xticks([0, 1])
-ax[0, 0].set_xticklabels(['easy', 'hard'])
+ax[0, 0].plot([0, 1, 2], mi.loc[cells].groupby(by='difficulty').mean(), 'o-', lw=2, color='k')
+ax[0, 0].set_xticks([0, 1, 2])
+ax[0, 0].set_xticklabels(['easy', 'medium', 'hard'])
 ax[0, 0].set_xlabel('Task block condition')
 ax[0, 0].set_ylabel(yaxis)
 ax[0, 0].axhline(0, linestyle='--', color='k')
 
-ax[0, 1].errorbar([0, 1], mi.loc[cells].groupby(by='difficulty').mean().values.squeeze(), \
+ax[0, 1].errorbar([0, 1, 2], mi.loc[cells].groupby(by='difficulty').mean().values.squeeze(), \
                             yerr=mi.loc[cells].groupby(by='difficulty').sem().values.squeeze(), lw=2, color='k')
-ax[0, 1].set_xticks([0, 1])
-ax[0, 1].set_xticklabels(['easy', 'hard'])
+ax[0, 1].set_xticks([0, 1, 2])
+ax[0, 1].set_xticklabels(['easy', 'medium', 'hard'])
 ax[0, 1].set_xlabel('Task block condition')
 ax[0, 1].set_ylabel(yaxis)
 ax[0, 1].axhline(0, linestyle='--', color='k')
@@ -136,20 +151,20 @@ mi = A1g[[yaxis]]
 cells = []
 for c in mi.index.get_level_values('cellid').unique():
     y = mi.loc[pd.IndexSlice[c, :], :].values
-    if len(y) > 1:
-        ax[1, 0].plot([0, 1], y, 'k', alpha=0.3)
+    if len(y) == 3:
+        ax[1, 0].plot([0, 1, 2], y, 'k', alpha=0.3)
         cells.append(c)
-ax[1, 0].plot([0, 1], mi.loc[cells].groupby(by='difficulty').mean(), 'o-', lw=2, color='k')
-ax[1, 0].set_xticks([0, 1])
-ax[1, 0].set_xticklabels(['easy', 'hard'])
+ax[1, 0].plot([0, 1, 2], mi.loc[cells].groupby(by='difficulty').mean(), 'o-', lw=2, color='k')
+ax[1, 0].set_xticks([0, 1, 2])
+ax[1, 0].set_xticklabels(['easy', 'medium', 'hard'])
 ax[1, 0].set_xlabel('Task block condition')
 ax[1, 0].set_ylabel(yaxis)
 ax[1, 0].axhline(0, linestyle='--', color='k')
 
-ax[1, 1].errorbar([0, 1], mi.loc[cells].groupby(by='difficulty').mean().values.squeeze(), \
+ax[1, 1].errorbar([0, 1, 2], mi.loc[cells].groupby(by='difficulty').mean().values.squeeze(), \
                             yerr=mi.loc[cells].groupby(by='difficulty').sem().values.squeeze(), lw=2, color='k')
-ax[1, 1].set_xticks([0, 1])
-ax[1, 1].set_xticklabels(['easy', 'hard'])
+ax[1, 1].set_xticks([0, 1, 2])
+ax[1, 1].set_xticklabels(['easy', 'medium', 'hard'])
 ax[1, 1].set_xlabel('Task block condition')
 ax[1, 1].set_ylabel(yaxis)
 ax[1, 1].axhline(0, linestyle='--', color='k')
