08ddf6acb6d490436c8f7e15405020fc25f9df2b
crheller <charlieheller95@gmail.com>
2020-06-18 16:22:51 -0700

    update pupil settings for new testing filepaths (locating under /auto/users/hellerc/)

--- nems_lbhb/pup_py2/pupil_settings.py
+++ nems_lbhb/pup_py2/pupil_settings.py
@@ -3,8 +3,10 @@ Path settings for where to store training data and model results
 """
 import os
 
+# Directory where raw video data is stored
 ROOT_VIDEO_DIRECTORY = '/auto/data/daq/'
 
-ROOT_DIRECTORY = '/auto/data/nems_db/pup_py/'
+# Directories for saving model results etc.
+ROOT_DIRECTORY = '/auto/users/hellerc/pup_py_testing/' #'/auto/data/nems_db/pup_py/'
 TRAIN_DATA_PATH = os.path.join(ROOT_DIRECTORY, 'training_data')
 TMP_SAVE = os.path.join(ROOT_DIRECTORY, 'tmp')
