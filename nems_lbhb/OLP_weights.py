e32ca8fa73e1ad965aafb2bb530d0f4224d4c5bf
Greg <gregory.hamersky@gmail.com>
2021-03-19 13:23:09 -0700

    Added cell exclusion criteria and some plotting functions for individual sound weights

--- nems_lbhb/OLP_weights.py
+++ nems_lbhb/OLP_weights.py
@@ -19,8 +19,7 @@ sb.color_palette
 sb.color_palette('colorblind')
 plt.rcParams['axes.prop_cycle'] = plt.cycler(color=sb.color_palette('colorblind')) 
 # OLP_cell_metrics_db_path='/auto/users/luke/Projects/OLP/NEMS/celldat_A1_v1.h5'
-OLP_cell_metrics_db_path = '/auto/users/hamersky/olp_analysis/a1_celldat1.h5'
-OLP_cell_metrics_db_path = '/auto/users/hamersky/olp_analysis/peg_celldat1.h5'
+
 
 #Decide which cell:
 #UPDATE THE LINE BELOW TO POINT TO THE FILE
@@ -30,6 +29,10 @@ OLP_cell_metrics_db_path = '/auto/users/hamersky/olp_analysis/peg_celldat1.h5'
 #rec_file = rec_file_dir + rf
 
 batch=328
+if batch == 328:
+    OLP_cell_metrics_db_path = '/auto/users/hamersky/olp_analysis/a1_celldat1.h5'
+if batch == 329:
+    OLP_cell_metrics_db_path = '/auto/users/hamersky/olp_analysis/peg_celldat1.h5'
 cell_df=nd.get_batch_cells(batch)
 cell_list=cell_df['cellid'].tolist()
 cell_list = [cell for cell in cell_list if cell[:3] != 'HOD']
@@ -92,6 +95,26 @@ cols=['EP_A','EP_B','IP_A','IP_B','SR','SR_av_std']
 cols2=cols+['SinglesMax','MEnh_I','MSupp_I','Rtype']
 df[cols2]
 
+##subtract metrics
+#check if my thresholds are reliably getting rid of the same 'bad' units
+the_data = df
+corco_df = df.loc[df['corcoef'] >= 0.1]
+bad_corco = df.loc[df['corcoef'] < 0.1]
+avg_df = df.loc[df['avg_resp'] >= 0.025]
+bad_avg = df.loc[df['avg_resp'] < 0.025]
+snr_df = df.loc[df['snr'] >= 0.03]
+bad_snr = df.loc[df['snr'] < 0.03]
+
+bad_corc_set = set(bad_corco.index)
+bad_avg_set = set(bad_avg.index)
+bad_snr_set = set(bad_snr.index)
+consistent_bad = bad_corc_set.intersection(bad_avg_set)
+bad_set = bad_corc_set.union(bad_avg_set)
+consistent_bad = bad_corc_set.intersection(bad_avg_set, bad_snr_set)
+bad_set = bad_corc_set.union(bad_avg_set, bad_snr_set)
+
+#filter dataframe
+df = df.loc[(df['corcoef'] >= 0.1) & (df['avg_resp'] >= 0.025)]
 
 Wcols=['namesA','namesB','weightsA','weightsB']
 weight_df = pd.concat(df['weight_dfR'].values,keys=df.index)
@@ -107,13 +130,13 @@ plt.xlim((-2, 2))
 plt.ylim((-2, 2))
 plt.gca().set_aspect(1)
 
-# weights=np.concatenate(df.weightsR.values,axis=1)
-# weights=weights[:,~np.any(np.isnan(weights),axis=0)]
-# plt.figure();  plt.hist2d(weights[0,:],weights[1,:],bins=bins)
-# plt.xlim((-.5, 1.5)); plt.ylim((-.5, 1.5))
-# plt.gca().set_aspect(1)
-# plt.xlabel('Background Weights')
-# plt.ylabel('Foreground Weights')
+weights=np.concatenate(df.weightsR.values,axis=1)
+weights=weights[:,~np.any(np.isnan(weights),axis=0)]
+plt.figure();  plt.hist2d(weights[0,:],weights[1,:],bins=bins)
+plt.xlim((-.5, 1.5)); plt.ylim((-.5, 1.5))
+plt.gca().set_aspect(1)
+plt.xlabel('Background Weights')
+plt.ylabel('Foreground Weights')
 
 #Same plot as the previous one but using the weight dataframe
 bins=np.arange(-2,2,.05)
@@ -299,3 +322,146 @@ ax = ax.ravel('F')
 ax[0].hist(snrs.values, bins=75), ax[0].set_title('SNR', fontweight='bold')
 ax[1].hist(coefs.values, bins=75), ax[1].set_title('Corr Coef', fontweight='bold')
 ax[2].hist(avgs.values, bins=75), ax[2].set_title('Average Response', fontweight='bold')
+fig.suptitle(f'Batch {batch}', fontweight='bold')
+
+#Make interactive scatterplot of weights
+X = df['corcoef']
+Y = df['avg_resp']
+idx_df = df
+cellid_and_str_strs= [index[0]+':'+nameA+':'+nameB for index,nameA,nameB in \
+                      zip(weight_df.index.values,
+                          weight_df['namesA'],weight_df['namesB'])]
+# coefs = [df['corcoef'][cell.split(':')[0]] for cell in cellid_and_str_strs]
+# avgs = [df['avg_resp'][cell.split(':')[0]] for cell in cellid_and_str_strs]
+# s2nrs = [df['snr'][cell.split(':')[0]] for cell in cellid_and_str_strs]
+#
+# cellid_and_str_strs = [orig+':'+coe+':'+avg+':'+s2 for orig,coe,avg,s2 in \
+#                        zip(cellid_and_str_strs, str(coefs), str(avgs), str(s2nrs))]
+
+f, ax = plt.subplots(1,1)
+fnargs={'plot_error':False}
+phi=ts.scatterplot_print(weight_df['weightsA'].values,
+                         weight_df['weightsB'].values,
+                         cellid_and_str_strs,
+                         ax=ax,fn=plot_psth,fnargs=fnargs)
+
+
+def plot_psth(cellid_and_stim_str, weight_df=weight_df, df=df, plot_error=True):
+    if plot_error:
+        nr = 2
+    else:
+        nr = 1
+    f, ax = plt.subplots(nr, 1)
+    if nr == 1: ax = [ax]
+    cellid, stimA, stimB = cellid_and_stim_str.split(':')
+    cell_df = weight_df.loc[cellid]
+    this_cell_stim = cell_df[(cell_df['namesA'] == stimA) & (cell_df['namesB'] == stimB)].iloc[0]
+    err = this_cell_stim['get_error']()
+    time = np.arange(0, err.shape[-1]) / fs
+    ax[0].plot(time, this_cell_stim['get_error'](get_what='sigA'))
+    ax[0].plot(time, this_cell_stim['get_error'](get_what='sigB'))
+    ax[0].plot(time, this_cell_stim['get_error'](get_what='sigAB'))
+    ax[0].plot(time, this_cell_stim['get_error'](get_what='pred'))
+    ax[0].legend(('Bg, weight={:.2f}'.format(this_cell_stim.weightsA),
+                  'Fg, weight={:.2f}'.format(this_cell_stim.weightsB),
+                  'Both',
+                  'Weight Model, r={:.2f}'.format(this_cell_stim.r)))
+    ax[0].set_title(f"{cellid_and_stim_str} -\ncorcoef: {df['corcoef'][cellid]} - "
+                    f"average response: {df['avg_resp'][cellid]} - snr: {df['snr'][cellid]}")
+
+    if plot_error:
+        ax[1].plot(time, this_cell_stim['get_error']() / np.sqrt(this_cell_stim['nf']))
+
+
+##Plot weights by individual sound
+import seaborn as sns
+import joblib as jl
+from pathlib import Path
+
+cell_list = df.index.values.tolist()
+
+sound_weights = pd.DataFrame()
+
+for cellid in cell_list:
+    bb = df.namesAR[cellid]
+    ff = df.namesBR[cellid]
+    ww = df.weightsR[cellid]
+
+    # bb = df.namesAR['ARM017a-01-10']
+    # ff = df.namesBR['ARM017a-01-10']
+    # ww = df.weightsR['ARM017a-01-10']
+
+    bgs = [bg.split('_', 1)[1].replace('_null', '').split('-', 1)[0] for bg in bb]
+    fgs = [fg.split('_', 1)[1].replace('null_', '').split('-', 1)[0] for fg in ff]
+    bgs_idx = [int(bg[:2]) for bg in bgs]
+    fgs_idx = [int(fg[:2]) for fg in fgs]
+
+    bg_weights = pd.DataFrame(
+        {'name': bgs,
+         'type': 'bg',
+         'idx': bgs_idx,
+         'weight': ww[0, :]
+         })
+    fg_weights = pd.DataFrame(
+        {'name': fgs,
+         'type': 'fg',
+         'idx': fgs_idx,
+         'weight': ww[1, :]
+         })
+
+    bgfg_weight = pd.concat([bg_weights, fg_weights], ignore_index=True)
+
+    sound_weights = sound_weights.append(bgfg_weight, ignore_index=True)
+
+sound_weights = sound_weights.sort_values(['type', 'idx'], ascending=[True, True])
+
+PEG_path = Path('/auto/users/hamersky/olp_analysis/PEG_weights')
+A1_path = Path('/auto/users/hamersky/olp_analysis/A1_weights')
+if PEG_path.parent.exists() is False and batch == 329:
+    PEG_path.parent.mkdir()
+if A1_path.parent.exists() is False and batch == 328:
+    A1_path.parent.mkdir()
+if batch == 329:
+    jl.dump(sound_weights, PEG_path)
+if batch == 328:
+    jl.dump(sound_weights, A1_path)
+
+if batch == 329:
+    peg_sound_weights = jl.load(PEG_path)
+if batch == 328:
+    a1_sound_weights = jl.load(A1_path)
+
+
+fig, ax = plt.subplots()
+# g = sns.stripplot(x='name', y='weight', hue='type', data=sound_weights, ax=ax)
+if batch == 329:
+    g = sns.stripplot(x='name', y='weight', hue='type', data=peg_sound_weights, ax=ax)
+    ax.set_ylim(-3, 2.5)
+    ax.set_title('PEG', fontweight='bold')
+    mean_weights = peg_sound_weights.groupby(by='name').agg('mean')
+    type_weights = peg_sound_weights.groupby(by='type').agg('mean')
+
+if batch == 328:
+    g = sns.stripplot(x='name', y='weight', hue='type', data=a1_sound_weights, ax=ax)
+    ax.set_ylim(-3, 2.5)
+    ax.set_title('A1', fontweight='bold')
+    mean_weights = a1_sound_weights.groupby(by='name').agg('mean')
+    type_weights = a1_sound_weights.groupby(by='type').agg('mean')
+
+bg_count = sound_weights.loc[sound_weights['type'] == 'bg']['name'].nunique()
+fg_count = sound_weights.loc[sound_weights['type'] == 'fg']['name'].nunique()
+
+labels = [e.get_text() for e in plt.gca().get_xticklabels()]
+ticks = plt.gca().get_xticks()
+w = 0.4
+for cnt, sound_name in enumerate(labels):
+    plt.hlines(mean_weights.loc[sound_name]['weight'],
+               ticks[cnt] - w, ticks[cnt] + w, lw=3, color='black')
+plt.hlines(type_weights.loc[type_weights.index == 'bg']['weight'],
+           -0.5, bg_count - 0.5, lw=2, linestyles=':', color='deepskyblue')
+plt.hlines(type_weights.loc[type_weights.index == 'fg']['weight'],
+           bg_count - 0.5, (bg_count+fg_count) - 0.5, lw=2, linestyles=':',
+           color='yellowgreen')
+
+g.set_xticklabels(g.get_xticklabels(), rotation=60)
+ax.set_ylim(bottom=-0.75, top=1.75)
