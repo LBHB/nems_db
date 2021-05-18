fixed pareto plot nparms calculation for full-recording models

nems_lbhb/plots.py
@@ -1392,7 +1392,7 @@ def LN_pop_plot(ctx, ctx0=None):
 
 def model_comp_pareto(modelnames=None, batch=0, modelgroups=None, goodcells=None,
                       offset=None, dot_colors=None, dot_markers=None, max=None, ax=None,
-                      check_single_cell=False, plot_stat='r_test'):
+                      check_single_cell=False, plot_stat='r_test', mean_per_model=False):
 
     if (modelnames is None) and (modelgroups is None):
         raise ValueError("Must specify modelnames list or modelgroups dict")
@@ -1449,7 +1449,10 @@ def model_comp_pareto(modelnames=None, batch=0, modelgroups=None, goodcells=None
 
     cellids = b_n.index.tolist()
     siteids = list(set([c.split("-")[0] for c in cellids]))
-    mean_cells_per_site = len(cellids)/len(siteids)
+    if mean_per_model:
+        mean_cells_per_site = len(cellids)
+    else:
+        mean_cells_per_site = len(cellids)/len(siteids)
     n_parms = np.array([np.mean(b_n[m]) for m in modelnames])
 
     # don't divide by cells per site if only one cell was fit