704d48851a062cc10e664161bab0fd3a6a8226dc
jacobpennington <jacob.pennington@ohsu.edu>
2021-05-25 10:29:29 -0700

    fixed typo in heldout cell analysis

--- nems_lbhb/xform_wrappers.py
+++ nems_lbhb/xform_wrappers.py
@@ -42,7 +42,7 @@ def _matching_cells(batch=289, siteid=None, alt_cells_available=None,
     if batch==289:
        pmodelname = "ozgf.fs100.ch18-ld-sev_dlog-wc.18x3.g-fir.3x15-lvl.1-dexp.1_init-basic"
     else:
-       pmodelname = "ozgf.fs100.ch18-ld-norm.l1-sev_wc.18x4R.g-fir.4x25xR-lvl.R-dexp.R_tfinit.n.lr1e3.et3.rb5.es20-newtf.n.lr1e4.es20"
+       pmodelname = "ozgf.fs100.ch18.pop-ld-norm.l1-popev_wc.18x4R.g-fir.4x25xR-lvl.R-dexp.R_tfinit.n.lr1e3.et3.rb5.es20-newtf.n.lr1e4.es20"
 
     single_perf = nd.batch_comp(batch=batch, modelnames=[pmodelname], stat='r_test')
     if alt_cells_available is not None:
