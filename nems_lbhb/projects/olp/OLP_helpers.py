46ddcdc94a1ebfb90874b9ac75ab5a7ef88404b5
Greg <gregory.hamersky@gmail.com>
2022-05-19 15:29:07 -0700

    many OLP plots for DAC and fitting

--- nems_lbhb/projects/olp/OLP_helpers.py
+++ nems_lbhb/projects/olp/OLP_helpers.py
@@ -1,6 +1,14 @@
 import numpy as np
 import scipy.stats as sst
 import pandas as pd
+from nems_lbhb.baphy_experiment import BAPHYExperiment
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from nems.analysis.gammatone.gtgram import gtgram
+from scipy.io import wavfile
+import glob
+import seaborn as sb
 
 def manual_fix_units(cell_list):
     '''I don't know why these units are incorrectly saved. But it was just these two, add
@@ -287,7 +295,215 @@ def get_binaural_adjacent_epochs(stim):
         return dA, dB
 
 
-
-
+def quadrants_by_FR(weight_df, threshold=0.05, quad_return=5):
+    '''Filters a dataframe by a FR threshold with spont subtracted. quad_returns says which
+    filtered quadrants to return. If you give a list it'll output as a dictionary with keys
+    which quadrant, if a single integer it'll just be the dataframe outputted. Default is 5
+    which takes a combination of BG+/FG+, BG+/FG-, and BG-/FG+.'''
+    quad1 = weight_df.loc[(weight_df.bg_FR<=-threshold) & (weight_df.fg_FR>=threshold)]
+    quad2 = weight_df.loc[(np.abs(weight_df.bg_FR)<=threshold) & (weight_df.fg_FR>=threshold)]
+    quad3 = weight_df.loc[(weight_df.bg_FR>=threshold) & (weight_df.fg_FR>=threshold)]
+    quad4 = weight_df.loc[(weight_df.bg_FR<=-threshold) & (np.abs(weight_df.fg_FR)<=threshold)]
+    quad6 = weight_df.loc[(weight_df.bg_FR>=threshold) & (np.abs(weight_df.fg_FR)<=threshold)]
+    quad10 = pd.concat([quad2, quad3, quad6], axis=0)
+    quad7 = weight_df.loc[(weight_df.bg_FR<=-threshold) & (weight_df.fg_FR<=-threshold)]
+    quad8 = weight_df.loc[(np.abs(weight_df.bg_FR)<=threshold) & (weight_df.fg_FR<=-threshold)]
+    quad9 = weight_df.loc[(weight_df.bg_FR>=threshold) & (weight_df.fg_FR<=-threshold)]
+    quad5 = weight_df.loc[(np.abs(weight_df.bg_FR)<threshold) & (np.abs(weight_df.fg_FR)<threshold)]
+    dfs = [quad1, quad2, quad3, quad4, quad5, quad6, quad7, quad8, quad9, quad10]
+    if isinstance(quad_return, list):
+        quads = {qq:dfs[qq - 1] for qq in quad_return}
+    elif isinstance(quad_return, int):
+        quads = dfs[quad_return - 1]
     else:
-        raise ValueError(f"Your stimulus is labeled as binaural but isn't.")
+        raise ValueError(f"quad_return input {quad_return} is not a list or int.")
+
+    return quads, threshold
+
+
+def histogram_subplot_handler(df_dict, yax='cells', tags=None):
+    if not tags:
+        tags = [ta for ta in df_dict.keys()]
+    dfs = [qu for qu in df_dict.values()]
+    if len(dfs) == 9:
+        fig, axes = plt.subplots(3, 3, sharex=True, sharey=False, figsize=(8, 8))
+    elif len(dfs) == 4:
+        fig, axes = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(8, 8))
+    else:
+        fig, axes = plt.subplots(1, len(dfs), sharex=True, sharey=True, figsize=(8, len(dfs)+1))
+
+    ax = axes.ravel()
+    for aa, tt, qq in zip(ax, tags, dfs):
+        weight_hist(qq, tag=tt, y=yax, ax=aa)
+
+
+def weight_hist(df, tag=None, y='cells', ax=None):
+    edges=np.arange(-2,2,.05)
+    if not ax:
+        fig, ax = plt.subplots()
+        title = True
+    else:
+        title = False
+
+    if y == 'cells':
+        ax.hist(df.weightsA, bins=edges, histtype='step')
+        ax.hist(df.weightsB, bins=edges, histtype='step')
+        ax.set_ylabel('Number of Cells', fontweight='bold', fontsize=8)
+    elif y == 'percent':
+        na, xa = np.histogram(df.weightsA, bins=edges)
+        na = na / na.sum() * 100
+        nb, xb = np.histogram(df.weightsB, bins=edges)
+        nb = nb / nb.sum() * 100
+        ax.hist(xa[:-1], xa, weights=na, histtype='step')
+        ax.hist(xb[:-1], xb, weights=nb, histtype='step')
+        ax.set_ylabel('Percent', fontweight='bold', fontsize=8)
+    else:
+        raise ValueError(f"y value {y} is not supported, put either 'cells' or 'percent'")
+
+    ax.set_xlabel('Weight', fontweight='bold', fontsize=8)
+    if title == True:
+        ax.set_title(f"{str(df.Animal.unique())} - {tag}", fontweight='bold', fontsize=12)
+        ax.legend(('Background', 'Foreground'), fontsize=7)
+    if title == False:
+        ax.set_title(f'{tag}', fontweight='bold', fontsize=8)
+        ax.legend(('Background', 'Foreground'), fontsize=4)
+
+
+def get_sound_statistics(weight_df, plot=True):
+    '''5/12/22 Takes a cellid and batch and figures out all the sounds that were played
+    in that experiment and calculates some stastistics it plots side by side. Also outputs
+    those numbers in a cumbersome dataframe'''
+    lfreq, hfreq, bins = 100, 24000, 48
+    cid, btch = weight_df.cellid.iloc[0], weight_df.batch.iloc[0]
+    manager = BAPHYExperiment(cellid=cid, batch=btch)
+    expt_params = manager.get_baphy_exptparams()  # Using Charlie's manager
+    ref_handle = expt_params[-1]['TrialObject'][1]['ReferenceHandle'][1]
+    BG_folder, FG_folder = ref_handle['BG_Folder'], ref_handle['FG_Folder']
+
+    bbs = list(set([bb.split('_')[1][:2] for bb in weight_df.epoch]))
+    ffs = list(set([ff.split('_')[2][:2] for ff in weight_df.epoch]))
+    bbs.sort(key=int), ffs.sort(key=int)
+
+    bg_paths = [glob.glob((f'/auto/users/hamersky/baphy/Config/lbhb/SoundObjects/@OverlappingPairs/'
+                           f'{BG_folder}/{bb}*.wav'))[0] for bb in bbs]
+    fg_paths = [glob.glob((f'/auto/users/hamersky/baphy/Config/lbhb/SoundObjects/@OverlappingPairs/'
+                           f'{FG_folder}/{ff}*.wav'))[0] for ff in ffs]
+    paths = bg_paths + fg_paths
+    bgname = [bb.split('/')[-1].split('.')[0] for bb in bg_paths]
+    fgname = [ff.split('/')[-1].split('.')[0] for ff in fg_paths]
+    names = bgname + fgname
+
+    Bs, Fs = ['BG'] * len(bgname), ['FG'] * len(fgname)
+    labels = Bs + Fs
+
+    sounds = []
+    means = np.empty((bins, len(names)))
+    means[:] = np.NaN
+    for cnt, sn, pth, ll in zip(range(len(labels)), names, paths, labels):
+        sfs, W = wavfile.read(pth)
+        spec = gtgram(W, sfs, 0.02, 0.01, bins, lfreq, hfreq)
+
+        dev = np.std(spec, axis=1)
+
+        freq_mean = np.nanmean(spec, axis=1)
+        x_freq = np.logspace(np.log2(lfreq), np.log2(hfreq), num=bins, base=2)
+        csm = np.cumsum(freq_mean)
+        big = np.max(csm)
+
+        freq75 = x_freq[np.abs(csm - (big * 0.75)).argmin()]
+        freq25 = x_freq[np.abs(csm - (big * 0.25)).argmin()]
+        freq50 = x_freq[np.abs(csm - (big * 0.5)).argmin()]
+        bandw = np.log2(freq75 / freq25)
+
+        means[:, cnt] = freq_mean
+
+        sounds.append({'name': sn,
+                       'type': ll,
+                       'std': dev,
+                       'bandwidth': bandw,
+                       '75th': freq75,
+                       '25th': freq25,
+                       'center': freq50,
+                       'spec': spec,
+                       'mean_freq': freq_mean,
+                       'freq_stationary': np.std(freq_mean)})
+
+    sound_df = pd.DataFrame(sounds)
+
+    # allmean = np.nanmean(means, axis=1, keepdims=True)
+    # norm_mean = [aa / allmean for aa in sound_df.mean_freq]
+    # freq_stationarity = [np.std(aa) for aa in allmean]
+    # sound_df['norm_mean'],  = norm_mean
+    # sound_df['freq_stationary'] = freq_stationarity
+
+    ss = sound_df.explode('std')
+    # frs = sound_df.explode('norm_mean')
+    # frs = sound_df.explode('mean_freq')
+    snames = [dd[2:] for dd in sound_df.name]
+
+    if plot:
+        fig, ax = plt.subplots(1, 3, figsize=(18, 8))
+
+        sb.barplot(x='name', y='std', palette=["lightskyblue" if x == 'BG' else 'yellowgreen' for x in sound_df.type],
+                   data=ss, ci=68, ax=ax[0], errwidth=1)
+        ax[0].set_xticklabels(snames, rotation=90, fontweight='bold', fontsize=7)
+        ax[0].set_ylabel('Non-stationariness', fontweight='bold', fontsize=12)
+        ax[0].spines['top'].set_visible(True), ax[0].spines['right'].set_visible(True)
+        ax[0].set(xlabel=None)
+
+        sb.barplot(x='name', y='bandwidth',
+                   palette=["lightskyblue" if x == 'BG' else 'yellowgreen' for x in sound_df.type],
+                   data=sound_df, ax=ax[1])
+        ax[1].set_xticklabels(snames, rotation=90, fontweight='bold', fontsize=7)
+        ax[1].set_ylabel('Bandwidth (octaves)', fontweight='bold', fontsize=12)
+        ax[1].spines['top'].set_visible(True), ax[1].spines['right'].set_visible(True)
+        ax[1].set(xlabel=None)
+
+        sb.barplot(x='name', y='freq_stationary',
+                   palette=["lightskyblue" if x == 'BG' else 'yellowgreen' for x in sound_df.type],
+                   data=sound_df, ax=ax[2])
+        ax[2].set_xticklabels(snames, rotation=90, fontweight='bold', fontsize=7)
+        ax[2].set_ylabel('Frequency Non-stationariness', fontweight='bold', fontsize=12)
+        ax[2].spines['top'].set_visible(True), ax[2].spines['right'].set_visible(True)
+        ax[2].set(xlabel=None)
+
+        fig.tight_layout()
+
+    return sound_df
+
+
+def add_sound_stats(weight_df, sound_df):
+    BGdf, FGdf = sound_df.loc[sound_df.type == 'BG'], sound_df.loc[sound_df.type == 'FG']
+    BGmerge, FGmerge = pd.DataFrame(), pd.DataFrame()
+    BGmerge['BG'] = [aa[2:].replace(' ', '') for aa in BGdf.name]
+    BGmerge['BG_Tstationary'] = [np.nanmean(aa) for aa in BGdf['std']]
+    BGmerge['BG_bandwidth'] = BGdf.bandwidth.tolist()
+    BGmerge['BG_Fstationary'] = BGdf.freq_stationary.tolist()
+
+    FGmerge['FG'] = [aa[2:].replace(' ', '') for aa in FGdf.name]
+    FGmerge['FG_Tstationary'] = [np.nanmean(aa) for aa in FGdf['std']]
+    FGmerge['FG_bandwidth'] = FGdf.bandwidth.tolist()
+    FGmerge['FG_Fstationary'] = FGdf.freq_stationary.tolist()
+
+    weight_df = pd.merge(right=BGmerge, left=weight_df, on=['BG'], validate='m:1')
+    weight_df = pd.merge(right=FGmerge, left=weight_df, on=['FG'], validate='m:1')
+
+    return weight_df
+
+
+def plot_example_specs(sound_df, sound_idx, lfreq=100, hfreq=24000, bins=48):
+    more = int(np.ceil(len(sound_idx)/2))
+    x_freq = np.logspace(np.log2(lfreq), np.log2(hfreq), num=bins, base=2)
+    ticks = [0, 10, 20, 30, 40, bins-1]
+    labels = [int(np.ceil(x_freq[aa])) for aa in ticks]
+
+    f, axes = plt.subplots(2, more, sharex=True, sharey=True, figsize=(8, 3*more))
+    ax = axes.ravel(order='F')
+    for AX, ii in zip(ax, sound_idx):
+        row = sound_df.iloc[ii]
+        AX.imshow(row['spec'], aspect='auto', origin='lower')
+        AX.set_yticks(ticks)
+        AX.set_yticklabels(labels)
+        AX.set_ylabel("Frequency (Hz)", fontweight='bold', fontsize=8)
+        AX.set_xlabel("Time (s)", fontweight='bold', fontsize=8)
+        AX.set_title(f"{row.type}: {row['name'][2:]}", fontweight='bold', fontsize=10)
