06f52de9e2c9802752659d0a247940a65566ccb3
Stephen David <stephen.v.david@gmail.com>
2023-09-11 17:27:50 -0700

    adding bilateral correlation script stub

--- nems_lbhb/projects/nat_streaming/site_corrs.py
+++ nems_lbhb/projects/nat_streaming/site_corrs.py
@@ -454,8 +454,8 @@ for siteid in siteids:
                         E = np.zeros((Xt.shape[0], Xt.shape[0]))
                         for aa in range(Xt.shape[0]):
                             for bb in range(Xt.shape[0]):
-                                #E[aa,bb]=np.corrcoef(Xt[aa], Xt[bb])[0,1]
-                                E[aa, bb] = np.std(X[aa,:]-X[bb,:])
+                                E[aa,bb]=1 - np.corrcoef(Xt[aa], Xt[bb])[0,1]
+                                #E[aa, bb] = np.std(X[aa,:]-X[bb,:])/np.sqrt((np.std(X[aa,:])*np.std(X[bb,:])))
                         np.fill_diagonal(E, np.nan)
                         #paircc = np.zeros((shuffle_count, cluster_count, int(ecount * (ecount - 1) / 2), 3))
                         nt = x1t.shape[1]
@@ -609,6 +609,9 @@ dfd=dfd.merge(df[['siteid','cid','estim','relgain']].loc[df['cid']==0],
               how='left',left_on=['siteid','s2'], right_on=['siteid','estim'], suffixes=('_1','_2'))
 dfd['mrelgain']=(dfd['relgain_1']+dfd['relgain_2'])/2
 
+cc=(dfd['cidx']<=cluster_count) & (np.isfinite(dfd['ccdiff'])) & \
+   (np.abs(dfd['ccdiff'])<3)
+
 f=plt.figure()
 ax=f.add_subplot(4,1,1)
 dfd[['s1s2ccmin','s1s2ccmax']].plot(lw=0.5, ax=ax)
@@ -616,7 +619,6 @@ dfd[['s1s2cc']].plot(color='red', ax=ax)
 dfd[['mean_cc']].plot(ls='--', lw=0.5, ax=ax)
 
 ax=f.add_subplot(4,3,4)
-cc=(dfd['cidx']<=cluster_count) & (np.isfinite(dfd['ccdiff']))
 sns.regplot(dfd.loc[cc], x='mean_cc', y='ccdiff',
             fit_reg=True, ax=ax, scatter_kws={'s': 3})
 ax.set_title(f"r={np.corrcoef(dfd.loc[cc,'mean_cc'], dfd.loc[cc,'ccdiff'])[0,1]:.3}")
@@ -626,7 +628,7 @@ sns.regplot(dfd.loc[cc], x='mean_cc', y='ccdiff2',
 ax.set_title(f"r={np.corrcoef(dfd.loc[cc,'mean_cc'], dfd.loc[cc,'ccdiff2'])[0,1]:.3}")
 ax=f.add_subplot(4,3,6)
 sns.scatterplot(dfd.loc[cc], x='s1s2shmean', y='s1s2cc', ax=ax, s=5)
-ax.plot([0,1], [0,1], 'k--', lw=0.5)
+ax.plot([0.5,1], [0.5,1], 'k--', lw=0.5)
 
 
 hi = np.sum(dfd['s1s2cc']>dfd['s1s2ccmax'])
@@ -640,11 +642,11 @@ dfd[['mpmin','mpmax']].plot(lw=0.5, ax=ax)
 dfd[['mp']].plot(color='red', ax=ax)
 dfd[['mean_cc']].plot(ls='--', lw=0.5, ax=ax)
 
-ax=f.add_subplot(4,2,7)
+ax=f.add_subplot(4,3,10)
 cc=dfd['cidx']<=cluster_count
 sns.regplot(dfd.loc[cc], x='mean_cc', y='mpdiff',
             fit_reg=True, ax=ax, scatter_kws={'s': 3})
-ax=f.add_subplot(4,2,8)
+ax=f.add_subplot(4,3,11)
 sns.regplot(dfd.loc[cc], x='cluster_n', y='mpdiff',
             fit_reg=True, ax=ax, scatter_kws={'s': 3})
 
